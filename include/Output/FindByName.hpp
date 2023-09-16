f2a87fd57a24eaec081c5740a5b3f66d237521c8
NDRAEY <andrejpavlenko666@gmail.com>
2023-09-16 21:41:26 +0900

    Support for dynamic module adding

--- include/Output/FindByName.hpp
+++ include/Output/FindByName.hpp
@@ -7,6 +7,20 @@
 
 #include "../Base.hpp"
 
+#include "../../include/Output/FindByName.hpp"
+#include "../../include/Output/Debug.hpp"
+#include "../../include/Output/FramebufferIO.hpp"
+#include "../../include/Output/TerminalIO.hpp"
+
 namespace Output {
+	typedef Output* (*outfunc_t)(struct ::PlayerInfo*);
+	
+	static std::vector<std::pair<std::string, outfunc_t>> func_mapping = {
+		{"None", nullptr},
+		{"Debug", &Debug::create},
+		{"Framebuffer", &FramebufferIO::create},
+		{"Terminal", &TerminalIO::create}
+	};
+
 	Output *FindByName(const std::string& name, struct ::PlayerInfo* player_info);
 }
